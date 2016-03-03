7e1faf7dfb679ff6814936493e0097d1aa9347e2
billy falconry ingram <billy.ingram16@hotmail.com>
2016-03-03 15:34:43 -0500

    The Beggining

---
+++ FixMyItunes/Album.cs
@@ -0,0 +1,81 @@
+﻿namespace FixMyItunes
+{
+    internal class Album
+    {
+        private string artist;
+
+        public string Artist
+        {
+            get { return artist; }
+            set { artist = value; }
+        }
+
+        private string albumName;
+
+        public string AlbumName
+        {
+            get { return albumName; }
+            set { albumName = value; }
+        }
+
+        private string trackTitle;
+
+        public string TrackTitle
+        {
+            get { return trackTitle; }
+            set { trackTitle = value; }
+        }
+
+        private string sortArtist;
+
+        public string SortArtist
+        {
+            get { return sortArtist; }
+            set { sortArtist = value; }
+        }
+
+        private string sortAlbum;
+
+        public string SortAlbum
+        {
+            get { return sortAlbum; }
+            set { sortAlbum = value; }
+        }
+
+        private string year;
+
+        public string Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        private int discNumber;
+
+        public int DiscNumber
+        {
+            get { return discNumber; }
+            set { discNumber = value; }
+        }
+
+        //constructor for Base List
+        public Album(string artist, string album)
+        {
+            this.Artist = artist;
+            this.AlbumName = album;
+        }
+
+        public Album(string artist, string album, string title, string sortArtist, string sortAlbum, string year, int discNumber)
+        {
+            this.Artist = artist;
+            this.SortAlbum = sortAlbum;
+            this.SortArtist = sortArtist;
+            this.TrackTitle = trackTitle;
+            this.Year = year;
+            this.AlbumName = albumName;
+            this.DiscNumber = discNumber;
+        }
+
+
+    }
+}
---
+++ FixMyItunes/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FixMyItunes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            DialogResult dr = fbd.ShowDialog();
+
+            txtFilePath.Text = fbd.SelectedPath;
+            this.btnStart.Visibility = Visibility.Visible;
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void RecursiveFileBuild (List<Album> list)
+        {
+
+        }
+    }
+
+   
+}
